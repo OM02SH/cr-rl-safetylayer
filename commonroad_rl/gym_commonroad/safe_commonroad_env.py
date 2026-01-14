b07f4b798f0ca2c7fa097161a029960f5ea2d8c0
Omar Shaat <omar.shaat@tum.de>
2026-01-14 11:33:19 +0100

    debug

--- commonroad_rl/gym_commonroad/safe_commonroad_env.py
+++ commonroad_rl/gym_commonroad/safe_commonroad_env.py
@@ -435,7 +435,6 @@ class SafetyLayer(CommonroadEnv):
         center_points = ct.reference_path_original
         ego_pos = np.asarray(self.observation_collector._ego_state.position).reshape(1 ,2)
         print(ego_pos.shape)
-        print(len(center_points))
         for k in center_points: print(k)
         closest_centerpoint = center_points[np.linalg.norm(center_points - ego_pos, axis=1).argmin()]
         self.safety_verifier = SafetyVerifier(self.scenario,self.prop_ego,self.precomputed_lane_polygons)
