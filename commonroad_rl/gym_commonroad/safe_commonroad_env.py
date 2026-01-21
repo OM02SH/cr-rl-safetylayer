74de78b81b1d88adada71df6fd3e4be50b1b0bef
Omar Shaat <omar.shaat@tum.de>
2026-01-21 16:49:58 +0100

    debug

--- commonroad_rl/gym_commonroad/safe_commonroad_env.py
+++ commonroad_rl/gym_commonroad/safe_commonroad_env.py
@@ -763,13 +763,9 @@ class SafetyLayer(CommonroadEnv):
         cy = center_points[:, 1]
         path_yaw = np.unwrap(np.arctan2(np.gradient(cy), np.gradient(cx)))
         desired_angle,_,_ = self.stanley_controller.stanley_control(p[0],p[1],yaw,v,steering_angle,cx,cy,path_yaw)
-        print("desired_angle:",desired_angle)
-        print("steering_angle:",steering_angle)
-        sv = (np.clip(desired_angle, -3, 3) - steering_angle)
-        print(sv)
-        res = ((np.clip(sv, -0.4, 0.4) / 0.4) * 1000 - self.ego_action._rescale_bias[0]) /self.ego_action._rescale_factor[0]
-        print(res)
-        return float(res)
+        sv = (desired_angle - steering_angle)
+        print("steering verlocity : ", sv)
+        return (float(np.clip(sv, -0.4, 0.4)) - self.ego_action._rescale_bias[0]) /self.ego_action._rescale_factor[0]
 
     def find_safe_acceleration(self, sv):
         """
