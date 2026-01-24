d245a0fe35cd0d76a9be89c6d9f1558e9ae36c34
Omar Shaat <omar.shaat@tum.de>
2026-01-24 15:11:50 +0100

    debug

--- commonroad_rl/gym_commonroad/safe_commonroad_env.py
+++ commonroad_rl/gym_commonroad/safe_commonroad_env.py
@@ -468,7 +468,7 @@ class SafetyVerifier:
         if q == 8:
             print(f"Safe action : {jd} on {ego_action.vehicle.state}")
             return True
-        # print(f"checking safe action : {jd},{kdd} on {ego_action.vehicle.state} now with depth {q}")
+        print(f"checking safe action : {jd},{kdd} on {ego_action.vehicle.state} now with depth {q}")
         q += 1
         ego_action.step(np.array([jd,kdd]))
         new_vehicle_state = ego_action.vehicle.state
@@ -537,7 +537,7 @@ class SafetyLayer(CommonroadEnv):
                          logging_path, test_env, play, config_file, logging_mode, **kwargs)
         self.observation = None
         self.past_ids = []
-        self.prop_ego = {"ego_length" : 4.5, "ego_width" : 1.61 , "a_lat_max" : 9.0, "a_lon_max" : 11.5, "delta_react" : 0.5}
+        self.prop_ego = {"ego_length" : 2.5, "ego_width" : 1.61 , "a_lat_max" : 9.0, "a_lon_max" : 11.5, "delta_react" : 0.5}
         self.time_step = 0
         self.lane_width = 5
         self.last_relative_heading = 0
