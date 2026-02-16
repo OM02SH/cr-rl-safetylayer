c9a2f20599780c46a0f2349612eed008416adb43
Omar Shaat <omar.shaat@tum.de>
2026-02-16 23:18:28 +0100

    old rewards

--- commonroad_rl/gym_commonroad/safe_commonroad_env.py
+++ commonroad_rl/gym_commonroad/safe_commonroad_env.py
@@ -904,9 +904,9 @@ class SafetyLayer(CommonroadEnv):
         self.in_or_entering_intersection = self.intersection_check()
         if terminated and self.time_step <= 2:
             if self.observation["is_off_road"][0] == True: 
-                reward+=1000
+                reward+=150
             elif self.observation["is_goal_reached"][0] == True:
-                reward-=2000
+                reward-=300
         if self.time_step % 5 == 0:
             self.safety_verifier.safeDistanceSet(self.observation_collector.ego_lanelet,self.in_or_entering_intersection,self.observation_collector._ego_state)
         else: self.safety_verifier.time_step += 1
